63e6925f4e56c996137d50967d102bac5dc4146e
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-09-20 15:19:57 -0400

    initial commit

---
+++ src/main/java/org/cicirello/math/stats/package-info.java
@@ -0,0 +1,29 @@
+/*
+ * rho mu - A Java library of randomization enhancements and other math utilities.
+ * Copyright 2017-2019 Vincent A. Cicirello, <https://www.cicirello.org/>.
+ *
+ * This file is part of the rho mu library.
+ *
+ * The rho mu library is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your 
+ * option) any later version.
+ *
+ * The rho mu library is distributed in the hope 
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even 
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the rho mu library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ 
+/**
+ * Collection of classes related to statistics. 
+ */
+package org.cicirello.math.stats;
+
+
