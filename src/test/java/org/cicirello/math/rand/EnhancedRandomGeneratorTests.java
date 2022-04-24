ce7160eaba165c46575a8bda16d8c986792d7827
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2022-04-24 14:11:41 -0400

    tests of delegation to wrapped rng

---
+++ src/test/java/org/cicirello/math/rand/EnhancedRandomGeneratorTests.java
@@ -0,0 +1,143 @@
+/*
+ * rho mu - A Java library of randomization enhancements and other math utilities.
+ * Copyright (C) 2017-2022 Vincent A. Cicirello, <https://www.cicirello.org/>.
+ *
+ * This file is part of the rho mu library.
+ *
+ * The rho mu library is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your 
+ * option) any later version.
+ *
+ * The rho mu library is distributed in the hope 
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even 
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the rho mu library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.cicirello.math.rand;
+
+import java.util.random.RandomGenerator;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * JUnit tests for the methods of the EnhancedRandomGenerator class.
+ */
+public class EnhancedRandomGeneratorTests {
+	
+	@Test
+	public void testDelegation() {
+		EnhancedRandomGenerator e = new EnhancedRandomGenerator(new FakeRNG());
+		assertEquals(4201, e.nextLong());
+		assertEquals(4202, e.nextLong(5000));
+		assertEquals(4203, e.nextLong(4000, 5000));
+		assertEquals(32, e.nextInt());
+		assertEquals(4204.0f, e.nextFloat(), 0.0f);
+		assertEquals(4205.0f, e.nextFloat(5000), 0.0f);
+		assertEquals(4206.0f, e.nextFloat(4000, 5000), 0.0f);
+		assertEquals(4207.0, e.nextExponential(), 0.0);
+		assertEquals(0.0, e.nextDouble(), 0.0);
+		assertEquals(Math.ulp(0.0), e.nextDouble(5000), 0.0);
+		assertEquals(2.0*Math.ulp(0.0), e.nextDouble(0, 5000), 0.0);
+		byte[] b = new byte[2];
+		e.nextBytes(b);
+		assertEquals(0x42, b[0]);
+		assertEquals(0x43, b[1]);
+		assertTrue(e.nextBoolean());
+	}
+	
+	@Test
+	public void testNoDelegation() {
+		EnhancedRandomGenerator e = new EnhancedRandomGenerator(new FakeRNG());
+		e.nextInt(128);
+		e.nextInt(0, 128);
+		e.nextGaussian();
+		e.nextGaussian(10, 1);
+	}
+	
+	private static class FakeRNG implements RandomGenerator {
+		
+		@Override public long nextLong() {
+			return 4201;
+		}
+		
+		@Override public long nextLong(long bound) {
+			return 4202;
+		}
+		
+		@Override public long nextLong(long origin, long bound) {
+			return 4203;
+		}
+		
+		@Override public int nextInt() {
+			return 32;
+		}
+		
+		@Override public int nextInt(int bound) {
+			fail("Should not delegate nextInt(bound)");
+			return 32;
+		}
+		
+		@Override public int nextInt(int origin, int bound) {
+			fail("Should not delegate nextInt(origin, bound)");
+			return 32;
+		}
+		
+		@Override public double nextGaussian() {
+			fail("Should not delegate nextGaussian()");
+			return 0.0;
+		}
+		
+		@Override public double nextGaussian(double mean, double stdev) {
+			fail("Should not delegate nextGaussian(mean, stdev)");
+			return mean;
+		}
+		
+		@Override public float nextFloat() {
+			return 4204.0f;
+		}
+		
+		@Override public float nextFloat(float bound) {
+			return 4205.0f;
+		}
+		
+		@Override public float nextFloat(float origin, float bound) {
+			return 4206.0f;
+		}
+		
+		@Override public double nextExponential() {
+			return 4207.0;
+		}
+		
+		@Override public double nextDouble() {
+			return 0.0;
+		}
+		
+		@Override public double nextDouble(double bound) {
+			return Math.ulp(0.0);
+		}
+		
+		@Override public double nextDouble(double origin, double bound) {
+			return 2*Math.ulp(0.0);
+		}
+		
+		@Override public void nextBytes(byte[] bytes) {
+			bytes[0] = 0x42;
+			bytes[1] = 0x43;
+		}
+		
+		@Override public boolean nextBoolean() {
+			return true;
+		}
+	}
+}
